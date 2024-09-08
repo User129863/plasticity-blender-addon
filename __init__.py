1ee793432999fe8ea05f0aee7049e7ec645b3502
User129863 <118719142+User129863@users.noreply.github.com>
2024-09-08 03:50:36 -0400

    Update __init__.py
    - Blender version update to 4.1

--- __init__.py
+++ __init__.py
@@ -12,8 +12,8 @@ bl_info = {
     "name": "Plasticity",
     "description": "A bridge to Plasticity",
     "author": "Nick Kallen, User129863",
-    "version": (1, 0, 6, 3),
-    "blender": (2, 80, 0),
+    "version": (2, 1, 0),
+    "blender": (4, 1, 0),
     "location": "View3D > Sidebar > Plasticity",
     "category": "Object",
 }
